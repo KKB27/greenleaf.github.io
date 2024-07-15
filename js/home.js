d43c1a819f7753d023d49701ab2d9b27e76fdd7d
As-star-at <agarwalashmi2005@gmail.com>
2024-07-15 22:12:42 +0530

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,6 +1,6 @@
 // Function to load the sign-in page content
 function loadSignInPage() {
-    fetch('Sign_in.html')
+    fetch('html/Sign_in.html')
         .then(response => response.text())
         .then(data => {
             // Inject the fetched content into the sign-in container
@@ -14,7 +14,7 @@ function loadSignInPage() {
 
             // Manually add the sign-in JavaScript to the document
             const signInScript = document.createElement('script');
-            signInScript.src = 'js/Sign_in.js';
+            signInScript.src = 'Sign_in.js';
             document.body.appendChild(signInScript);
         })
         .catch(error => console.error('Error loading sign-in page:', error));
